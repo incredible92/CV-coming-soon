fb656986ffc5ac6e4a5108cc74ea9e553359d7a6
incredible92 <sulymanabdullahi@gmail.com>
2021-01-05 00:14:22 +0100

    fix: responsiveness

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="app">
       <div className=" flex  ">
           <div className="mt-3 "  >
-           <div className="ml-40"> <img  className="w-16 md:w-32 lg:w-48 "src={logo} alt="logo"/></div>
+           <div className="ml-40"> <img className=" w-16  lg:w-40" src={logo} alt="logo"/></div>
             
-            <h1 className="mt-3 text-white text-5xl">WE ARE COMING SOON...</h1>
+            <h1 className="mt-3  text-white md:text-2xl lg:text-5xl">WE ARE COMING SOON...</h1>
           </div>
          
       </div>
