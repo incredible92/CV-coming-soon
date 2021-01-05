432241c4876cb9da972549cdb2254b8f84e5c7c9
incredible92 <sulymanabdullahi@gmail.com>
2021-01-05 17:26:03 +0100

    fix: comingSoon

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="app">
       <div className=" flex  ">
-          <div className="mt-3 "  >
-           <div className="ml-40"> <img className=" w-16  lg:w-40" src={logo} alt="logo"/></div>
+          <div className="mt-3 text-center "  >
+           <div className=""> <img className=" w-16  lg:w-40" src={logo} alt="logo"/></div>
             
-            <h1 className="mt-3  text-white md:text-2xl lg:text-5xl">WE ARE COMING SOON...</h1>
+            <h1 className="mt-3 text-white md:text-5xl lg:text-5xl">WE ARE COMING SOON...</h1>
           </div>
          
       </div>
